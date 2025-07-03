8dec17b492d72555939c8af22dc2f7d795a9cc8a
Alexandr Zuev <zuevalex14021983@gmail.com>
2025-07-03 21:57:35 +0500

    desctop open/close catalog btn

--- js/scroll-header.js
+++ js/scroll-header.js
@@ -4,11 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const headerMiddle = header.querySelector('.header__middle');
   const headerDown = header.querySelector('.header__down');
   const heroSection = document.querySelector('.hero');
+  const catalog = document.querySelector('.catalog-list');
+  const openCatalogBtn = document.querySelector('.catalog-open');
+  const closeCatalogBtn = document.querySelector('.catalog-close');
 
   let headerTopHeight = headerTop.offsetHeight;
   let headerDownHeight = headerDown.offsetHeight;
   let headerMiddleHeight = headerMiddle.offsetHeight;
   let scrollTop = 0;
+
+    catalog.style.top = `${header.offsetHeight}px`;
 
   window.addEventListener('scroll', () => {
     scrollTop = window.scrollY;
@@ -16,9 +21,31 @@ document.addEventListener("DOMContentLoaded", () => {
       headerMiddle.classList.add('pos-fixed');
       heroSection.style.marginTop = `(${headerDownHeight} + ${headerMiddleHeight})px`;
       headerDown.style.marginTop = `-${headerDownHeight}px`;
+      catalog.style.top = `${headerMiddleHeight}px`;
+      
     } else {
       headerMiddle.classList.remove('pos-fixed');
       headerDown.style.marginTop = '';
+      catalog.style.top = `${header.offsetHeight}px`;
     }
   })
+
+  const openCatalog = function() {
+    catalog.style.display = 'flex';
+    openCatalogBtn.style.display = 'none';
+    closeCatalogBtn.style.display = 'flex';
+  }
+  
+  const closeCatalog = function() {
+    catalog.style.display = 'none';
+    closeCatalogBtn.style.display = 'none';
+    openCatalogBtn.style.display = 'flex';
+    
+  }
+  
+
+  openCatalogBtn.addEventListener('click', openCatalog);
+  closeCatalogBtn.addEventListener('click', closeCatalog);
+  
+
 })
